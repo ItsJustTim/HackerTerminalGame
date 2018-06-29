ab1ed96970e2d3b73c524bd22a6d362ec7d4b6ed
ItsJustTim <40666715+ItsJustTim@users.noreply.github.com>
2018-06-29 09:02:59 +0100

    Add files via upload

--- Hacker.cs
+++ Hacker.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
 
     // Game configuration data
-    string[] level1Passwords = { "books", "aisle", "self", "password", "font", "borrow" };
+    const string menuHint = "You may type 'menu' at any time.";
+    string[] level1Passwords = { "books", "aisle", "shelf", "password", "font", "borrow" };
     string[] level2Passwords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
 
     // Game state
@@ -37,6 +35,7 @@ public class Hacker : MonoBehaviour
         if (input == "menu") // we can always go direct to main menu
         {
             ShowMainMenu();
+            menuHint = "something else";
         }
         else if (currentScreen == Screen.MainMenu)
         {
@@ -50,44 +49,100 @@ public class Hacker : MonoBehaviour
 
     void RunMainMenu(string input)
     {
-        if (input == "1")
+        bool isValidLevelNumber = (input == "1" || input == "2");
+        if (isValidLevelNumber)
         {
-            level = 1;
-            password = level1Passwords[2]; // todo make random later
-            StartGame();
+            level = int.Parse(input);
+            AskForPassword();
         }
-        else if (input == "2")
-        {
-            level = 2;
-            password = level2Passwords[4];
-            StartGame();
-        }
-        else if (input == "007")
+        else if (input == "007") // easter egg
         {
             Terminal.WriteLine("Please select a level Mr Bond!");
         }
         else
         {
             Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine(menuHint); 
         }
     }
 
-    void StartGame()
+    void AskForPassword()
     {
         currentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen level " + level);
-        Terminal.WriteLine("Please enter your password: ");
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Enter your password, hint: " + password.Anagram());
+        Terminal.WriteLine(menuHint);
+    }
+
+    void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                int index0 = Random.Range(0, level1Passwords.Length);
+                password = level1Passwords[index0];
+                break;
+            case 2:
+                int index1 = Random.Range(0, level2Passwords.Length);
+                password = level2Passwords[index1];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
     }
 
     void CheckPassword(string input)
     {
         if (input == password)
         {
-            Terminal.WriteLine("WELL DONE!");
+            DisplayWinScreen();
         }
         else
         {
-            Terminal.WriteLine("Sorry, wrong password!");
+           AskForPassword();
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+        Terminal.WriteLine(menuHint);
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Here's a book...");
+                Terminal.WriteLine(@"  
+
+             _______
+            / it's /|    
+           /  a   //
+          / book //
+         (++++++)/
+          ``````
+        "
+               );
+               break;
+            case 2:
+                Terminal.WriteLine("The prison key...");
+                Terminal.WriteLine(@"
+                        ___
+          _____________/   \
+         /  __________  O  /
+         \_/          \___/
+          
+        "      );
+               break;
+            default:
+                Debug.LogError("Invalid level reached");
+               break;
         }
     }
 }
